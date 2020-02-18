cf583832b3c00ad3211206c1c14857092ab4159f
Jeferson Slywitch <jeferson.slywitch@gmail.com>
2020-02-17 23:36:07 -0300

    achando o erro

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'React'
+import React, {Component} from 'react'
 import { View, Text } from 'react-native'
 
-export default class App extends React.Component {
+export default class App extends Component {
     render() {
         return (
             <View>
