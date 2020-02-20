96db79456e5b22ffbb00595b6ac826eda0868328
Jeferson Slywitch <jeferson.slywitch@gmail.com>
2020-02-19 23:47:26 -0300

    continuando o codigo

--- src/Menu.js
+++ src/Menu.js
@@ -3,20 +3,32 @@ import { createDrawerNavigator } from 'react-navigation'
 import Simples from './componentes/Simples'
 import ParImpar from './componentes/Parimpar'
 import { Inverter, MegaSena } from './componentes/Multi'
+import Contador from './componentes/Contador'
+import Plataforma from './componentes/Plataforma'
+import ValidarProps from './componentes/ValidarProps'
 
 export default createDrawerNavigator({
+    ValidarProps: {
+        screen: () => <ValidarProps ano={20} />,
+    },
+    Plataforma: {
+        screen: () => <Plataforma />,
+    },
+    Contador: {
+        screen: () => <Contador numeroInicial={0} />,
+    },
     MegaSena:{
         screen: () => <MegaSena numeros={6} />,
         navigationOptions: { title: 'Mega Sena '}
     },
     Inverter:{
-        screen: () => <Inverter texto='React Native' />
+        screen: () => <Inverter texto='React Native' />,
     },
     ParImpar:{
         screen: () => <ParImpar numeros={36} />,
         navigationOptions: { title: 'Par & Impar '}
     },
     Simples:{
-        screen: () => <Simples texto='Flexivel!!!!' />
+        screen: () => <Simples texto='Flexivel!!!!' />,
     }
 }, {drawerWidth: 300 })
---
+++ src/componentes/Contador.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { View, Text, TouchableHighlight } from 'react-native'
+import Padrao from '../estilo/Padrao'
+
+export default class Contador extends Component {
+    
+    state = {
+        numero: this.props.numeroInicial
+    }
+    
+    maisUm = () => {
+        this.setState({ numero: this.state.numero + 1 })
+    }
+
+    limpar = () => {
+        this.setState({ numero: this.props.numeroInicial })
+    }
+
+    render () {
+        return (
+            <View>
+                <Text style={{fontSize: 40}}>{this.state.numero}</Text>
+                <TouchableHighlight
+                    onPress={this.maisUm}
+                    onLongPress={this.limpar}>
+                        <Text style={Padrao.ex}>Incrementar/Zerar</Text>
+                </TouchableHighlight>           
+            </View>
+        )
+    }
+}
